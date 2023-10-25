3048490de1edcd17ce8f1b1929e5951268548aa1
Anny <104777994+Any09@users.noreply.github.com>
2023-10-25 18:03:22 -0300

    rthsrt

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = TocaSomPom;
+document.querySelector('.tecla_pom').onclick=TocaSomPom;
+
+function TocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla-clap').onclick=TocaSomClap;
